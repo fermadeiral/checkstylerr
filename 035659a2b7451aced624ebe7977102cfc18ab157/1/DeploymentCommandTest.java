1e6c22c76c33a46fabd2b98de870315bc26c101a
Fer Madeiral <fer.madeiral@gmail.com>
2021-06-24 21:29:41 +0000

    [Automatic commit] Checkstyle violations from jenkinsci/kubernetes-cd-plugin

---
+++ 035659a2b7451aced624ebe7977102cfc18ab157/1/DeploymentCommandTest.java
@@ -0,0 +1,32 @@
+package com.microsoft.jenkins.kubernetes.command;
+
+import com.microsoft.jenkins.kubernetes.wrapper.KubernetesClientWrapper;
+import io.kubernetes.client.openapi.ApiClient;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests for {@link DeploymentCommand}
+ */
+public class DeploymentCommandTest {
+    private static final String FALLBACK_MASTER = "Unknown";
+
+    @Test
+    public void testGetMasterHost() {
+        assertEquals(FALLBACK_MASTER, DeploymentCommand.getMasterHost(null));
+
+        KubernetesClientWrapper wrapper = mock(KubernetesClientWrapper.class);
+        assertEquals(FALLBACK_MASTER, DeploymentCommand.getMasterHost(wrapper));
+
+        ApiClient client = mock(ApiClient.class);
+        when(wrapper.getClient()).thenReturn(client);
+        assertEquals(FALLBACK_MASTER, DeploymentCommand.getMasterHost(wrapper));
+
+        final String host = "some.host";
+        when(client.getBasePath()).thenReturn(host);
+        assertEquals(host, DeploymentCommand.getMasterHost(wrapper));
+    }
+}
---
+++ c0798b47be2e40ef027480dcfb7d923c0cd4df4c/1/KubernetesClientWrapperTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.microsoft.jenkins.kubernetes;
+
+import com.microsoft.jenkins.kubernetes.util.Constants;
+import com.microsoft.jenkins.kubernetes.wrapper.KubernetesClientWrapper;
+import hudson.EnvVars;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * Tests for {@link KubernetesClientWrapper}.
+ */
+public class KubernetesClientWrapperTest {
+    @Test
+    public void testPrepareSecretName() throws Exception {
+        final int lengthLimit = Constants.KUBERNETES_NAME_LENGTH_LIMIT;
+        assertEquals("ab", KubernetesClientWrapper.prepareSecretName("$var", "abc", new EnvVars("var", "ab")));
+        assertException(IllegalArgumentException.class, new Runnable() {
+            @Override
+            public void run() {
+                // length breach
+                KubernetesClientWrapper.prepareSecretName(new String(new char[lengthLimit + 1]).replace('\0', 'a'), "abc", new EnvVars());
+            }
+        });
+        assertException(IllegalArgumentException.class, new Runnable() {
+            @Override
+            public void run() {
+                // illegal name
+                KubernetesClientWrapper.prepareSecretName("Abc", "abc", new EnvVars());
+            }
+        });
+        assertTrue(KubernetesClientWrapper.prepareSecretName(null, null, new EnvVars()).startsWith(Constants.KUBERNETES_SECRET_NAME_PREFIX));
+        assertTrue(KubernetesClientWrapper.prepareSecretName(null, "abcd", new EnvVars()).startsWith(Constants.KUBERNETES_SECRET_NAME_PREFIX + "abcd"));
+        assertTrue(KubernetesClientWrapper.prepareSecretName(null, new String(new char[lengthLimit + 1]).replace('\0', 'a'), new EnvVars()).length() <= Constants.KUBERNETES_NAME_LENGTH_LIMIT);
+    }
+
+    private <T extends Exception> void assertException(Class<T> clazz, Runnable action) {
+        try {
+            action.run();
+            fail();
+        } catch (Exception e) {
+            if (!clazz.isAssignableFrom(e.getClass())) {
+                throw e;
+            }
+        }
+    }
+}
