661de5390bc81c855f4c9ee74bc19828b60ae6b8
Fer Madeiral <fer.madeiral@gmail.com>
2021-06-24 21:55:25 +0000

    [Automatic commit] Checkstyle violations from microsoft/azure-iot-edge-jenkins-plugin

---
+++ 19c40ec85c487b237d92777dac945b1c92395e38/2/UtilTest.java
@@ -0,0 +1,40 @@
+package com.microsoft.jenkins.iotedge;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by zhqqi on 7/27/2018.
+ */
+public class UtilTest {
+
+    @Test
+    public void targetConditionValidationTest() {
+        Assert.assertTrue(Util.isValidTargetCondition("tags.city='Shanghai'"));
+        Assert.assertTrue(Util.isValidTargetCondition("properties.reported.lastStatus='200'"));
+        Assert.assertTrue(Util.isValidTargetCondition("deviceId='device1'"));
+        Assert.assertFalse(Util.isValidTargetCondition("properties.desired.lastStatus='200'"));
+        Assert.assertFalse(Util.isValidTargetCondition("tags.='Shanghai'"));
+        Assert.assertFalse(Util.isValidTargetCondition("tag.stauts='200'"));
+    }
+
+    @Test
+    public void deploymentIdValidationTest() {
+        Assert.assertTrue(Util.isValidDeploymentId("abc"));
+        Assert.assertTrue(Util.isValidDeploymentId("a1b2"));
+        Assert.assertTrue(Util.isValidDeploymentId("a-:+??=5"));
+        Assert.assertFalse(Util.isValidDeploymentId(""));
+        Assert.assertFalse(Util.isValidDeploymentId("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"));
+        Assert.assertFalse(Util.isValidDeploymentId("ab$cd"));
+    }
+
+    @Test
+    public void priorityValidationTest() {
+        Assert.assertTrue(Util.isValidPriority("1"));
+        Assert.assertTrue(Util.isValidPriority("12"));
+        Assert.assertTrue(Util.isValidPriority("0"));
+        Assert.assertFalse(Util.isValidPriority("a1"));
+        Assert.assertFalse(Util.isValidPriority("one"));
+        Assert.assertFalse(Util.isValidPriority("123.4"));
+    }
+}
