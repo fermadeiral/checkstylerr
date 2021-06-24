2bd609d63d1f4bbdd04ad0d90b564453980fa1d7
Fer Madeiral <fer.madeiral@gmail.com>
2021-06-24 23:33:39 +0000

    [Automatic commit] Checkstyle violations from self-xdsd/todo-finder-cli

---
+++ 7db5c71cb391e91937cd57e5254faadb0cce1086/2/TodosWithBodies.java
@@ -0,0 +1,81 @@
+public class LegalTodos {
+
+    /**
+     * Method A.
+     *
+     * @todo #112:12min this is an example of a single-line todo.
+     */
+    public void methodA() {
+
+    }
+
+    /**
+     * Method B.
+     *
+     * FIXME #113:13min this is an example
+     *  of a multi-line
+     *  todo.
+     */
+    public void methodB() {
+
+    }
+
+    // TODO #114:14min small todo
+	public void methodC() {
+
+    }
+
+    /**
+     * Method D.
+     *
+     * @fixme #115:15min this todo
+	 *  should only be
+	 *  three lines long
+     * This line should not be included in the todo.
+     */
+    public void methodD() {
+
+    }
+
+    /**
+     * Method E.
+     *
+     * @todo #116:16min
+     *  this body is in the next line...
+     */
+    public void methodE() {
+
+    }
+
+
+    /**
+     * Method F.
+     *
+     * @todo #118:18min     ...body...
+     */
+    public void methodF() {
+
+    }
+
+    /**
+     * Method G.
+     *
+     *      @todo #200:30min Single line todo method G.
+     *  This line is not part of todo even if a space is aligned with start.
+     *
+     */
+    public void methodG() {
+
+    }
+
+    /**
+     * Method H.
+     *
+     * @todo #200:30min Single line todo method H.
+     *  @todo #201:30min This a new todo an not part of the above todo.
+     *
+     */
+    public void methodG() {
+
+    }
+}
---
+++ eaf6e6103c862163d3bbcbf0da62f1e3f35ec3d5/0/IllegalTodos.java
@@ -0,0 +1,28 @@
+/**
+ * @todo
+ */
+
+// TODO
+
+// ... @fixme ...
+
+/*
+ * FIXME
+ * @fixme
+ */
+
+//TODO
+
+// @todo
+
+/**
+ * TODO foo
+ */
+
+//TODO foo
+
+//TODO #30:foo
+
+//TODO #30:min
+
+//FIXME #30:days
